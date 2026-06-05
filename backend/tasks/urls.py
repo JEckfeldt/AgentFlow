795f5b0ca098f14dea4d055e7ce88d03ba69a13d
JEckfeldt <jaeckfeldt@outlook.com>
2026-06-05 01:36:46 -0700

    started user authentication

---
+++ backend/tasks/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from .views import ProjectViewSet, TaskViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(r'projects', ProjectViewSet)
+router.register(r'tasks', TaskViewSet)
+router.register(r'comments', CommentViewSet)
+
+urlpatterns = router.urls
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Project, Task, Comment
+from .serializers import ProjectSerializer, TaskSerializer, CommentSerializer
 
-# Create your views here.
+
+class ProjectViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
